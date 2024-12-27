27df2509d9f4b70c46ca95e797b88759ce6072c4
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-27 12:47:46 +0530

    created a submission

--- script.js
+++
@@ -1,7 +0,0 @@
-function hover(element) {
-  element.setAttribute('src', 'https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg');
-}
-
-function unhover(element) {
-  element.setAttribute('src', 'https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg');
-}
